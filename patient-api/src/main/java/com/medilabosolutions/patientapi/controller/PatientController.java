3e5297608ddbb10c960c1b4b66be6f328051e730
Rahul Jain <rahulj93.swe@gmail.com>
2026-01-13 05:23:32 -0500

    Add controller + service + repository layers

--- patient-api/src/main/java/com/medilabosolutions/patientapi/controller/PatientController.java
+++ patient-api/src/main/java/com/medilabosolutions/patientapi/controller/PatientController.java
@@ -1,34 +1,41 @@
 package com.medilabosolutions.patientapi.controller;
 
-import java.util.*;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.medilabosolutions.patientapi.model.Patient;
+import com.medilabosolutions.patientapi.service.PatientService;
 
 @RestController
 public class PatientController {
 
+    private final PatientService patientService; 
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService; 
+    }
+
     @GetMapping("/patients")
     public List<Patient> getPatients() {
-        Patient patient1 = new Patient("Patient", "Test", "12-Jan-2026", "M", "123 VS Code Rd", "111-111-1111"); 
-        Patient patient2 = new Patient("Patient2", "Test2", "13-Jan-2026", "F", "456 VS Code Rd", "000-000-0000"); 
-
-        return new ArrayList<Patient>(Arrays.asList(patient1, patient2)); 
+        return patientService.getPatients(); 
     }
 
     @GetMapping("/patient")
     public Patient getPatient(String lastName, String firstName) {
-        // logic to be added. this is a placeholder for now 
-        return new Patient(lastName, firstName, "12-Jan-2026", "M", "123 VS Code Rd", "111-111-1111"); 
+        return patientService.getPatient(lastName, firstName);  
     } 
     
     @PutMapping("/patient")
     public Patient updatePatient(String lastName, String firstName) {
-        return new Patient(); 
+        return patientService.updatePatient(lastName, firstName);  
     }
 
     @DeleteMapping("/patient")
     public List<Patient> deletePatient(String lastName, String firstName) {
-        return new ArrayList<>(); 
+        return patientService.deletePatient(lastName, firstName);
     }
-
 }
--- patient-api/src/main/java/com/medilabosolutions/patientapi/service/PatientService.java
+++ patient-api/src/main/java/com/medilabosolutions/patientapi/service/PatientService.java
@@ -1,5 +1,33 @@
 package com.medilabosolutions.patientapi.service;
 
-public class PatientService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.medilabosolutions.patientapi.model.Patient;
+import com.medilabosolutions.patientapi.repository.PatientRepository;
+
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+    
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository; 
+    }
+
+    public List<Patient> getPatients() {
+        return patientRepository.getPatients(); 
+    }
+
+    public Patient getPatient(String lastName, String firstName) {
+        return patientRepository.getPatient(lastName, firstName);
+    }
+
+    public Patient updatePatient(String lastName, String firstName) {
+        return patientRepository.updatePatient(lastName, firstName);
+    }
+
+    public List<Patient> deletePatient(String lastName, String firstName) {
+        return patientRepository.deletePatient(lastName, firstName);
+    }
 }
