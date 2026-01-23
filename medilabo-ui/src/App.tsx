21b60219de722b89a2593fcb70a588cc426b1b04
Rahul Jain <rahulj93.swe@gmail.com>
2026-01-23 01:59:42 -0500

    Connect UI to backend via ports 8080 and 8081

--- medilabo-ui/src/App.tsx
+++ medilabo-ui/src/App.tsx
@@ -1,73 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [patients, setPatients] = useState<any>({});
+  const [notes, setNotes] = useState<any[]>([]);
+
+  useEffect(() => {
+    // Fetch patient by query params
+    fetch("/patient?firstName=John&lastName=Doe")
+      .then(res => res.json())
+      .then(data => {
+        console.log("Patient:", data)
+        setPatients(data);
+      });
+
+    // Fetch all notes
+    fetch("/notes")
+      .then(res => res.json())
+      .then(data => {
+        console.log("Notes:", data)
+        setNotes(data);
+      });
+  }, [])
 
   return (
     <>
       <h1>Medilabo Solutions</h1>
-      <div style={{ width: '70vw', display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-      <form onSubmit={()=>{}}>
-        <h2>Patient</h2>
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-          <label>
-            Last Name: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's last name"/>
-          </label>
-          <label>
-            First Name: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's first name"/>
-          </label>
-          <label>
-            Date of birth: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's date of birth"/>
-          </label>
-          <label>
-            Gender: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient name's gender"/>
-          </label>
-          <label>
-            Address: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's address"/>
-          </label>
-          <label>
-            Phone: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's phone number"/>
-          </label>
-        </div>
-        <button type="submit">Get</button>
-        <button type="submit">Add</button>
-        <button type="submit">Edit</button>
-        <button type="submit">Delete</button>
-      </form>
-      <form onSubmit={()=>{}}>
-        <h2>Note</h2>
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-          <label>
-            ID: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter ID"/>
-          </label>
-          <label>
-            Patient Name: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's full name"/>
-          </label>
-          <label>
-            Note: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter your note"/>
-          </label>
-          <label>
-            patId: 
-            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's ID"/>
-          </label>
-        </div>
-        <button type="submit">Get</button>
-        <button type="submit">Add</button>
-        <button type="submit">Edit</button>
-        <button type="submit">Delete</button>
-      </form>
+      <div style={{ width: '70vw', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <form onSubmit={() => { }}>
+          <h2>Patient</h2>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label>
+              Last Name:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter patient's last name" />
+            </label>
+            <label>
+              First Name:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter patient's first name" />
+            </label>
+            <label>
+              Date of birth:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter patient's date of birth" />
+            </label>
+            <label>
+              Gender:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter patient name's gender" />
+            </label>
+            <label>
+              Address:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter patient's address" />
+            </label>
+            <label>
+              Phone:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter patient's phone number" />
+            </label>
+          </div>
+          <button type="submit">Lookup</button>
+          <button type="submit">Add</button>
+          <button type="submit">Edit</button>
+          <button type="submit">Delete</button>
+        </form>
+        <form onSubmit={() => { }}>
+          <h2>Note</h2>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label>
+              ID:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter ID" />
+            </label>
+            <label>
+              Patient Name:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter patient's full name" />
+            </label>
+            <label>
+              Note:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter your note" />
+            </label>
+            <label>
+              patId:
+              <input type="text" value={undefined} onChange={() => { }} placeholder="Enter patient's ID" />
+            </label>
+          </div>
+          <button type="submit">Lookup</button>
+          <button type="submit">Add</button>
+          <button type="submit">Edit</button>
+          <button type="submit">Delete</button>
+        </form>
+      </div>
+      <div>
+      {patients.lastName}, {patients.firstName} , {patients.dateOfBirth}, {patients.gender}, {patients.address}, {patients.phone}
+      </div>
+      <div>
+        {notes.map(note => (
+          <div>{note.patient}:  {note.note}</div>
+        ))}
       </div>
     </>
   )
--- medilabo-ui/vite.config.ts
+++ medilabo-ui/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5183, // Vite dev server port 
+    proxy: {
+      '/patient': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+      '/notes': {
+        target: 'http://localhost:8081',
+        changeOrigin: true 
+      }
+    }
+  }
 })
