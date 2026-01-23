ad7d80427d6231d0c20fdc71d7d14a5a1cbe1e9f
Rahul Jain <rahulj93.swe@gmail.com>
2026-01-23 01:31:35 -0500

    start creating basic UI

--- medilabo-ui/src/App.tsx
+++ medilabo-ui/src/App.tsx
@@ -8,26 +8,67 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Medilabo Solutions</h1>
+      <div style={{ width: '70vw', display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+      <form onSubmit={()=>{}}>
+        <h2>Patient</h2>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <label>
+            Last Name: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's last name"/>
+          </label>
+          <label>
+            First Name: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's first name"/>
+          </label>
+          <label>
+            Date of birth: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's date of birth"/>
+          </label>
+          <label>
+            Gender: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient name's gender"/>
+          </label>
+          <label>
+            Address: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's address"/>
+          </label>
+          <label>
+            Phone: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's phone number"/>
+          </label>
+        </div>
+        <button type="submit">Get</button>
+        <button type="submit">Add</button>
+        <button type="submit">Edit</button>
+        <button type="submit">Delete</button>
+      </form>
+      <form onSubmit={()=>{}}>
+        <h2>Note</h2>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <label>
+            ID: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter ID"/>
+          </label>
+          <label>
+            Patient Name: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's full name"/>
+          </label>
+          <label>
+            Note: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter your note"/>
+          </label>
+          <label>
+            patId: 
+            <input type="text" value={undefined} onChange={()=>{}} placeholder="Enter patient's ID"/>
+          </label>
+        </div>
+        <button type="submit">Get</button>
+        <button type="submit">Add</button>
+        <button type="submit">Edit</button>
+        <button type="submit">Delete</button>
+      </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
