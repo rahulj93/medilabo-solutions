5c28e7b222ab0714bd597ff458c4a7f1ee1b02a3
Rahul Jain <rahulj93.swe@gmail.com>
2026-03-05 01:54:12 -0500

    Use configuration properties in application.yml to define urls in  gateway service

--- gateway-service/src/main/java/com/medilabosolutions/gatewayservice/GatewayApplication.java
+++ gateway-service/src/main/java/com/medilabosolutions/gatewayservice/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.medilabosolutions.gatewayservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -8,6 +9,16 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
+
+    @Value("${PATIENT_API_URL}")
+    private String patientApiUrl;
+
+    @Value("${NOTES_API_URL}")
+    private String notesApiUrl;
+
+    @Value("${RISK_ASSESSMENT_API_URL}")
+    private String riskAssessmentApiUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
@@ -18,24 +29,29 @@ public class GatewayApplication {
             .route(
                 p -> p
                 .path("/patients")
-                .filters(f -> f.addRequestHeader("MedilaboSolutions", "Patients"))
-                .uri("http://localhost:8080")
+                // .filters(f -> f.addRequestHeader("MedilaboSolutions", "Patients"))
+                .uri(patientApiUrl)
             )
             .route(
                 p -> p
                 .path("/patient/**")
-                .filters(f -> f.addRequestHeader("Medilabo", "Patient"))
-                .uri("http://localhost:8080"))
+                // .filters(f -> f.addRequestHeader("Medilabo", "Patient"))
+                .uri(patientApiUrl))
             .route(
                 p -> p
                 .path("/notes/**")
-                .filters(f -> f.addRequestHeader("Patient", "Notes"))
-                .uri("http://localhost:8081"))
+                // .filters(f -> f.addRequestHeader("Patient", "Notes"))
+                .uri(notesApiUrl))
             .route(
                 p -> p
                 .path("/notes")
-                .filters(f -> f.addRequestHeader("Patient", "Notes"))
-                .uri("http://localhost:8081"))
+                // .filters(f -> f.addRequestHeader("Patient", "Notes"))
+                .uri(notesApiUrl))
+            .route(
+                p -> p
+                .path("/risk-assessment/**")
+                // .filters(f -> f.addRequestHeader("Patient", "Risk Assessment"))
+                .uri(riskAssessmentApiUrl))
             .build(); 
     }
 }
